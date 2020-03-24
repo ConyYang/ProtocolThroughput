c2a3ecb25639cf5c1488c7ec130713b13ad7644b
Yang Yubei <yangyubei0218@gmail.com>
2020-03-24 21:53:24 +0800

    divide functions

--- slottedAloha/plot.py
+++ slottedAloha/plot.py
@@ -1,21 +1,32 @@
-
 '''
-Basic model: no retransmissions, only mean arrival rate of the poisson distribution as a parameter.
+Plot the functions
 '''
 
 __author__ = "Yang Yubei"
 import numpy as np
-import math, os
+import math
 import matplotlib.pyplot as plt
 import matplotlib
-G = np.arange(start=0, stop=3, step=0.01)
-S = []
-for i in range(len(G)):
-    S.append(G[i]*(math.exp(-2*G[i])))
+import throughputFunc
 
-plt.plot(G, S)
 plt.title('Throughout for the various access modes', fontsize=10)
 plt.xlabel('G (Offered Channel Traffic)')
 plt.ylabel('S (Throughput)')
-plt.text(2, 0.04, 'Pure Aloha')
+
+G= np.arange(start=0, stop=10, step=0.01)
+
+S_pA = throughputFunc.pure_aloha(G)
+S_sA = throughputFunc.slotted_aloha(G)
+S_np = throughputFunc.nonP_CSMA(G)
+S_op = throughputFunc.oneP_CSMA(G)
+
+plt.plot(G, S_pA,'chocolate')
+plt.plot(G, S_sA,'crimson')
+plt.plot(G, S_np,'blueviolet')
+plt.plot(G, S_op,'lightseagreen')
+
+#plt.text(2, 0.04, 'Pure Aloha')
+#plt.text(2, 0.28, 'Slotted Aloha')
+#plt.text(3.0, 0.16, 'Non-Persistent CSMA')
+print(S_op)
 plt.show()
